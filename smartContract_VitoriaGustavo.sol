f60baedb65aefd447dce2e2fc03b9c3e082fa158
GustMont <gustavo.monteiro@sou.inteli.edu.br>
2023-03-01 17:18:43 -0300

    Depósito Inicial pelo Usuário - Gustavo e Vitória

--- smartContract_VitoriaGustavo.sol
+++ smartContract_VitoriaGustavo.sol
@@ -1,19 +1,29 @@
 //pedido para entrar no contrato 
 pragma solidity ^0.8.17;
+
 contract ContratoCoover {
     //estrutura de dados para armazenar as informações necessários para entrar em um contrato
     struct entrarContrato {
-        bytes32 hashImei; //retorna o valor do hash do IMEI (32 dígitos)
-        uint valorCelular; // valor referente ao celular do usuário
-        bool possuiCarteira; //se o usuário possui carteira no Metamask
+        IMEI; //retorna o valor do hash do IMEI (32 dígitos)
+        uint valorAtivo; // valor referente ao celular do usuário
+        carteiraUsuario; //se o usuário possui carteira no Metamask
     }
 
 // função que recebe e retorna as informações do backend (sem armazenamento no contrato, apenas na memória)
-    function retornarEntrarContrato(bytes32 hashImei, uint valorCelular, bool possuiCarteira) public pure returns (entrarContrato memory) {
-        return entrarContrato(hashImei, valorCelular, possuiCarteira);
+    function retornarEntrarContrato(_IMEI, uint _valorAtivo, _carteiraUsuario) public pure returns (entrarContrato memory) {
+        return entrarContrato(_IMEI, _valorAtivo, _carteiraUsuario);
+    }
+// função para retornar o valor do depósito inicial de um usuário a partir do valor do aparelho a ser segurado, acrescido do valor da taxa administrativa.
+    function primeiroDeposito(uint _taxAdmin, uint _valorAtivo){
+        uint depositoInicial; //variável que irá armazenar o valor a ser depositado
+        
+        depositoInicial = (_valorAtivo * 0.05) + _taxAdmin;// Deposito inicial = cobertura(5% do valor do celular) + tax admin
+        //fórmula para extrair 5% do valor total do aparelho segurado, acrescentando a taxa administrativa
+        
+        return depositoInicial;
+        //retornando o valor do depósito inicial que deve ser realizado
     }
 }
-
 
 // pragma solidity ^0.8.17;
 // contract ContratoCoover2 {
@@ -34,3 +44,5 @@ contract ContratoCoover {
 //         });
 //     }
 // }
+
+
