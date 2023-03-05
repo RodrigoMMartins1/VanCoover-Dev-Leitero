015c42f8ba516c7d17e77f91e99478eb3f4bad14
RodrigoMMartins1 <99209230+RodrigoMMartins1@users.noreply.github.com>
2023-03-05 19:29:37 -0300

    Update certo

--- smart.sol
+++ smart.sol
@@ -1,89 +1,282 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
-contract ContratoCoover {
-    // Variáveis fundamentais para a manipulação dos conteúdos dentro das funções.
-    uint public valorAtivo;
-    uint public saldoUsuario;
-    uint public saldoContrato;
-    uint public quantiUsuario;
-    uint public dataInicio;
-    uint public dataValidade;
-    uint public minPessoas = 5;
-    uint public maxPessoas = 50;
-    uint public duracaoDias;
-    uint public valorCobertura;
-    uint public totalContrato;
-    string private IMEI;
-    uint public taxAdmin;
-    uint public hashBo;
-    string[] private listaIMEIs;
-    address private _admin;
 
 
-    // Struct definido para armazenar os dados das carteiras associadas ao contrato, tendo como membro os endereços das carteiras.
-    struct Usuario {
-        address carteira;
-        string IMEI;
-        string bo;
-    }
+contract Coover {
+// Definição da struct Participante
+struct Participante {
 
-    }
-    struct Carteiras {
-        address carteiraUsuario;
-    }
-    // Armazena-se as carteiras em um array público para serem lidas como a variável carteira.
-    Carteiras[]  carteira;
+    uint256 saldo; // Saldo do Participante da seu carteira
+    bytes15 imei; // IMEI do celular do Participante que estará em hash
+    address payable endereco; //endereço dos Participante
+    uint256 valorAtivo; // Valor do celular do participante
+    bytes15 bo; //bo do participante que estará em hash
+    uint256 qntIndenizacao; //Quantidade de indenizações que um participante recebeu
+    bool pediuIndenizacao; // Diz se um participante solicitou ou não uma indenização
+    uint256 index; 
 
 
-    // Armazena-se as carteiras em um array público para serem lidas como a variável carteira.
-    Usuario[]  usuarios;
-    // Mapeamento do número de carteiras que estão associadas ao contrato.
-    mapping(address => bool)  termoAceito;
-    uint  numAceitaram;
-    // Mapeamento do valor inteiro, dentro do endereço indicado, para identificação do saldo pertecente ao contrato.
-    mapping (address => uint)  saldo;
-    uint  saldoTotal;
-
-    // Construtor utilizado para inicializar as variáveis de estado do contrato.
-    // Os argumentos utilizados no construtor são o saldo, quantidade de usurios, minimo de usuários para funcionamento, máximo possível de usuários e duração do contrato.
-    constructor(uint _saldoTotal, uint _usuarios, uint _usuarioMinimo, uint _usuarioMaximo, uint _duracaoDias) {
-        _admin = msg.sender; // _admin representa o remetende do contrato.
-        dataInicio = block.timestamp; // Armazena a data de inicialização do contrato.
-        saldoContrato = _saldoTotal; 
-        quantiUsuario = _usuarios;
-        minPessoas = _usuarioMinimo;
-        maxPessoas = _usuarioMaximo;
-        dataValidade = dataInicio + _duracaoDias * 1 days; // Guarda a data de validade.
-    }
-    // Funções 
-    // Modificador para permitir que apenas o dono do contrato (admin) acesse as funções.
-    modifier donoUnico() {
-        require(msg.sender == _admin, "Apenas o dono do contrato pode executar essa funcao.");
-        _;
-    }
-    // Funcionalidade: criar um novo contrato
-    function criarContrato(uint _dataInicio, uint _dataValidade, uint _taxaAdmin, uint _minPessoas, uint _maxPessoas) public {
-        require(_dataInicio < _dataValidade, "A data de inicio deve ser anterior a data de validade");
-        require(_minPessoas >= 2, "O minimo de pessoas deve ser maior ou igual a 2");
-        require(_maxPessoas <= 100, "O maximo de pessoas deve ser menor ou igual a 100");
-        require(_minPessoas <= _maxPessoas, "O minimo de pessoas deve ser menor ou igual ao maximo de pessoas");
-
-        dataInicio = _dataInicio;
-        dataValidade = _dataValidade;
-        taxAdmin = _taxaAdmin;
-        minPessoas = _minPessoas;
-        maxPessoas = _maxPessoas;
-    }
-    // Funcionalidade: Entrar no contrato.
-    function entrarContrato(address _carteira, string memory _imei /* preco celular */) public payable {
-     require(block.timestamp <= dataValidade, "Este contrato expirou");
-        require(usuarios.length < maxPessoas, "Este contrato ja atingiu o numero maximo de usuarios");
-
-        uint precoCelular = 1000; // definir o preço do celular aqui
-        uint precoTotal = precoCelular + (precoCelular * taxAdmin / 100);
-
-        require(msg.value == precoTotal, "O valor enviado nao corresponde ao valor total do celular com a taxa administrativa");
-
-        usuarios.push(Usuario(_carteira, _imei));
-        saldoContrato += precoCelular;
+   
 }
+
+// Definição da struct Grupo
+struct Grupo {
+    string nomeGrupo; // Nome do grupo de segurados
+    uint256 saldoTotal; // Saldo total do grupo no contrato
+    uint256 qntparticipantes; // Número de Participantes no grupo
+    uint256 minimoParticipantes; // Número mínimo de Participantes para o grupo
+    uint256 maximoParticipantes; // Número máximo de Participantes para o grupo
+    address[]  participantes; //Array com os endereços dos participantes
+    address payable seguradora; // endereço do criador do smart contract (owner)
+    uint256 taxAdmin; //Taxa para entrar no grupo
+    uint256 dataInicio; //Data de crição do contrato
+    uint256 dataValidade; //Data do término do contrato
+    uint256 duracaoDias; // Dias totais do contrato
+
+    mapping (address => Participante) mapeamentoParticipantes; // Mapeamento de Participantes por endereço
 }
+
+//Outras Variáveis que serão usadas no projeto
+
+
+//Eventos para serem usados mais tarde
+event novoParticipanteGrupo(address indexed participante, uint256 valor);
+event pedidoIndenizacao(address indexed participante, bytes15 bo);
+
+
+
+//Constructor/Deploy do contrato, definir as variáveis dos grupos
+ Grupo public meuGrupo; //Objeto do grupo
+
+ constructor(string memory _nomeGrupo, uint256 _minimoParticipantes, uint256 _maximoParticipantes, uint256 _taxAdmin, uint256 _duracaoDias)  {
+        meuGrupo.nomeGrupo = _nomeGrupo; //Nome do Grupo
+        meuGrupo.minimoParticipantes = _minimoParticipantes; //Mínimo e máximo de participantes
+        meuGrupo.maximoParticipantes = _maximoParticipantes; //Máximo de participantes
+        meuGrupo.taxAdmin = _taxAdmin; //Taxa de entrada
+        meuGrupo.duracaoDias = _duracaoDias; //Duração do contrato
+        meuGrupo.dataInicio = block.timestamp; // define a data de início do contrato como a data atual do bloco
+        meuGrupo.dataValidade = meuGrupo.dataInicio + meuGrupo.duracaoDias * 1 days; // Guarda a data de validade.
+        meuGrupo.seguradora =  payable(msg.sender); // define o endereço da seguradora como o criador do contrato
+
+    }
+
+
+
+
+
+//Funções(Regras de negócios)
+
+
+//Entrar no smart contract, requisitos imei, valor do ativo endereço da carteira do participante, colocar a função deposito inicial
+function entrarGrupo(bytes15 _imei, uint256 _valorAtivo, address payable  _endereco) external payable {
+    require(meuGrupo.qntparticipantes < meuGrupo.maximoParticipantes, "Grupo ja atingiu o limite maximo de participantes.");
+
+    require(msg.value == (_valorAtivo * 5 / 100) + meuGrupo.taxAdmin, "Deposito inicial incorreto.");
+    uint256 _qntIndenizacao = 0;
+    
+
+    
+    // Adiciona o participante no mapeamento de participantes
+    meuGrupo.mapeamentoParticipantes[_endereco] = Participante({
+        saldo: msg.value,
+        imei: _imei,
+        endereco: _endereco,
+        valorAtivo: _valorAtivo,
+        bo: "",
+        qntIndenizacao: _qntIndenizacao,
+        pediuIndenizacao: false,
+        index: meuGrupo.participantes.length - 1
+    });
+
+    
+
+    
+    // Adiciona o endereço do participante no array de participantes do grupo
+    meuGrupo.participantes.push(_endereco);
+    
+    // Incrementa o número de participantes do grupo
+    meuGrupo.qntparticipantes++;
+
+     // Transfere a taxa administrativa para a carteira da seguradora
+    meuGrupo.seguradora.transfer(meuGrupo.taxAdmin);
+
+    //Adiciona no saldo do Grupo
+    meuGrupo.saldoTotal += _valorAtivo * 5 / 100;
+    
+
+    //Adicionado a lista de participantes
+    emit novoParticipanteGrupo(_endereco, msg.value);
+}
+
+
+
+// Função para realizar um depósito de um participante no grupo
+function depositar() public payable {
+    //Instancia o participante que entrou no grupo
+    Participante storage participante = meuGrupo.mapeamentoParticipantes[msg.sender];
+
+    //Permite que só possa entrar so arquivo maior que 0
+    require(participante.valorAtivo > 0, "Participante nao encontrado");
+
+    //Deposito é o valor do ativo * 5 porcento
+    uint256 deposito = participante.valorAtivo * 5 / 100;
+
+    //Colaca o saldo do participante como deposito
+    participante.saldo = deposito;
+    
+
+    require(msg.value == deposito, "Valor de deposito incorreto");
+
+    meuGrupo.saldoTotal += deposito;
+}
+
+
+
+
+
+
+//Fazer pedido de indenização, provas de documento de sinistralidade(BO), tem q ter emit do evento para notificar o adm(segurado) com o bo do participante
+
+function solicitarIndenizacao( address _endereco, bytes15 _bo) public {
+    Participante storage participante = meuGrupo.mapeamentoParticipantes[_endereco];
+    require(participante.saldo >=   participante.valorAtivo * 5/100, "Saldo insuficiente para solicitar indenizacao");
+    require(participante.bo == _bo, "BO invalido");
+
+    emit pedidoIndenizacao(msg.sender, _bo);
+    participante.pediuIndenizacao = true;
+    
+}
+
+function indenizacao(address payable _participante, bytes15 _imei, uint256 _valorAtivo) public {
+    require(msg.sender == meuGrupo.seguradora, "Apenas a seguradora pode executar essa funcao");
+
+    //Instancia o participante a ser indenizado
+    Participante storage participanteIndenizado = meuGrupo.mapeamentoParticipantes[_participante];
+
+
+    //Verificando se o IMEI compátivel
+    require(participanteIndenizado.imei == _imei, "O IMEI nao e deste usuario");
+
+    //Verificando se o valor do ativo é compátivel
+    require(participanteIndenizado.valorAtivo == _valorAtivo, "O valor do ativo nao e deste usuario");
+
+    //Verifica se o participante pediu indenizacao
+    require(participanteIndenizado.pediuIndenizacao == true, "Participante nao solicitou indenizacao.");
+
+
+    //Retira o indenizado do Grupo, ele terá que entrar no grupo de novo para pedir indenizacao
+    delete meuGrupo.participantes[participanteIndenizado.index];
+
+    //Faz a conta para o valor total do ativo retirando o deposito do indenizado
+    uint256 valorParaContribuicao = 0;
+    valorParaContribuicao = participanteIndenizado.valorAtivo - (participanteIndenizado.saldo);
+
+    //Zera o valor de indenizado
+    participanteIndenizado.saldo = 0;
+
+    uint256 valorPago;
+
+
+     for(uint i=0; i<meuGrupo.participantes.length; i++) {
+
+
+         //Instancia participante do grupo para contribuicao
+         Participante storage participante = meuGrupo.mapeamentoParticipantes[meuGrupo.participantes[i]];
+         
+         //Contribuicao é o valor proporcional de cada participante para pagar o valor do Ativo do indenizado
+         uint256 contribuicao = (participante.valorAtivo * 5) / valorParaContribuicao;
+         
+         //Retira a contribuicao do saldo do participante
+         participante.saldo -= contribuicao;
+
+        //Itera o valor a ser pago
+
+        valorPago += contribuicao;
+
+
+     }
+
+    
+
+
+    //Transfere o valor indenizado
+     participanteIndenizado.endereco.transfer(valorPago);
+     
+     //Retira a solicatacao 
+     participanteIndenizado.pediuIndenizacao = false;
+
+     //Soma a quantidade de indenizacao
+     participanteIndenizado.qntIndenizacao += 1;
+
+}
+
+    
+
+//Renovar contrato
+function renovarContrato() public {
+
+    require(msg.sender == meuGrupo.seguradora, "Apenas seguradora pode executar essa funcao");
+    require(meuGrupo.qntparticipantes >= meuGrupo.minimoParticipantes, "Nao e possivel renovar o contrato pois o numero minimo de participantes nao foi alcancado.");
+    // Atualiza a data de validade do contrato
+    meuGrupo.dataValidade = block.timestamp + meuGrupo.duracaoDias * 1 days;
+}
+
+//Funções view 
+
+//Ver quantidade de pessoas do grupo
+function quantidadeParticipantes() public view returns (uint256) {
+    return meuGrupo.qntparticipantes;
+}
+
+//Ver quantidade máxima de participantes
+function maximoParticipantes() external view returns(uint256) {
+    return meuGrupo.maximoParticipantes;
+}
+
+//Numero de participantes
+function qntParticipantes() external view returns(uint256) {
+    return meuGrupo.qntparticipantes;
+}
+
+//Ver numero da taxa administrativa
+function taxaAdmin() external view returns(uint256) {
+    return meuGrupo.taxAdmin;
+}
+
+//Ver saldo do participante
+function saldoParticipante(address _endereco) external view returns(uint256) {
+    return meuGrupo.mapeamentoParticipantes[_endereco].saldo;
+}
+
+//Ver nome do Grupo
+function nomeGrupo() external view returns (string memory) {
+    return meuGrupo.nomeGrupo;
+}
+
+//Ver duração de dias
+function duracaoDias() external view returns (uint256) {
+    return meuGrupo.duracaoDias;
+}
+
+//Ver data de inicio
+function dataInicio() external view returns (uint256) {
+    return meuGrupo.dataInicio;
+}
+
+//Ver data de validade do Grupo
+function dataValidade() external view returns (uint256) {
+    return meuGrupo.dataValidade;
+}
+//Ver quantidade de indenização do participante
+function indenizacao(address _endereco) external view returns (uint256){
+    return meuGrupo.mapeamentoParticipantes[_endereco].qntIndenizacao;
+}
+
+
+}
+
+
+
+
+
+
